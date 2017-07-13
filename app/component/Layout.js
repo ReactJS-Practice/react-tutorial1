5be3c637d9a94108f9ab6d05f656a643bdb05a2b
unknown <sambitmishra987@gmail.com>
2017-07-14 00:09:45 +0530

    Tutorial on State:

--- app/component/Layout.js
+++ app/component/Layout.js
@@ -4,9 +4,18 @@ import Header from './Header';
 import Footer from './Footer';
 
 export default class Layout extends React.Component{
+  constructor(){
+  	super();
+  	this.state = {name: "Sambit"};
+  }
+
   render() {
+  	setTimeout(()=>{
+  		this.setState({name: "Swarup"});
+  	},1000)
     return (
     	<div>
+    		{this.state.name}
     		<Header/>
     		<div>Body....</div>
     		<Footer/>
