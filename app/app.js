421733e1bc5603b746b9f0915174f0ae45b2b153
unknown <sambitmishra987@gmail.com>
2017-07-13 10:39:04 +0530

    Reactjs Components & Rendering: with separate method in class getLastName()

--- app/app.js
+++ app/app.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Layout extends React.Component{
+  getLastName(){
+    return " Mishra";
+  }
+
   render() {
     const name = 'Sambit';
     return (
-      <h1> It's {name}!</h1>
+      <h1> It's {name} {this.getLastName()}!</h1>
     );
   }
 }
