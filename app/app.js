5503ffbf155cf173f7ca8feea5f6e586443c622e
unknown <sambitmishra987@gmail.com>
2017-07-14 12:45:25 +0530

    React-router SPA setup

--- app/app.js
+++ app/app.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, hasHistory} from "react-router"
+
+import Bootstrap from "./vendor/bootstrap-without-jquery";
 
 import Layout from './component/Layout';
 
 const app = document.getElementById('content');
-ReactDOM.render(<Layout/>, app);
+//ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+  <Router history={hasHistory}>
+    <Route path="/" component={Layout}>
+    </Route>
+  </Router>
+, app);
